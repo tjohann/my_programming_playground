34f86d230e4d6f2e0dd678bab44a9aed05b9c0a5
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2020-12-06 13:50:36 +0100

    add some more content related to c startup some cleanups and addtional docs/infos

---
+++ c_startup/hello_test.c
@@ -0,0 +1,20 @@
+/*
+  simple hello world example with an test function to see how linker
+  handles function signatures
+ */
+
+#include <stdio.h>
+
+int
+test(void)
+{
+	printf("hello world\n");
+
+	return 0;
+}
+
+int
+main(void)
+{
+	return test();
+}
