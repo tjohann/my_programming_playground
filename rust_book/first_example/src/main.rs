b35265f3a30cdfddead81cc213059ff868b1486d
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2022-11-09 23:08:29 +0100

    add first complete code example in rust

--- rust_book/first_example/src/main.rs
+++ rust_book/first_example/src/main.rs
@@ -1,3 +1,36 @@
+use std::str::FromStr;
+use std::env;
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+	if m < n {
+	    let t = m;
+	    m = n;
+	    n = t;
+	}
+	m = m %n;
+    }
+    n
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+	numbers.push(u64::from_str(&arg)
+		     .expect ("error parsing arguments"));
+    }
+
+    if numbers.len() == 0 {
+	eprintln!("Usage: gcd NUMBER ...");
+	std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+	d = gcd(d, *m);
+    }
+
+    println!("The gcd if {:?} is {}", numbers, d);
 }
