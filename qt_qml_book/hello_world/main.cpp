eb4feeaf97a923f6f7e26bd038a5a6f9d453e5e4
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2024-03-13 11:53:07 +0100

    add qtcreator hello_world example

---
+++ qt_qml_book/hello_world/main.cpp
@@ -0,0 +1,25 @@
+#include "main_window.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "hello_world_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    main_window w;
+    w.show();
+    return a.exec();
+}
