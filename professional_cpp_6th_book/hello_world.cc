7ab3f7c8f83d6d71d887b0c7383a186d0a760d5d
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2024-11-18 21:05:10 +0100

    up to side 11 in the book add input-set to UTF-8

--- professional_cpp_6th_book/hello_world.cc
+++ professional_cpp_6th_book/hello_world.cc
@@ -1,14 +1,42 @@
 /*
- * hello world zum checken ob alles läuft
+ * up to side 11
+ */
+
+/*
+ * import std;
+ * or
+ * import <print>;
+ * wont be fully supported with gcc-14.2
  */
 
 #include <print>
 #include <string>
+#include <iostream>
+
+namespace my_func {
+	// no need to use std::
+	using namespace std;
+
+	void foo(void)
+	{
+		println("In {}", __FUNCTION__);
+	}
+}
+
+using namespace my_func;
 
 int main(void)
 {
 	std::string s {"Hallo \"Professional C++\" Buch :-)"};
 	std::println("{}", s);
+
+	// std::cerr
+	std::println(std::cerr, "Error code: {}", -1);
+
+	// within the namespaces using scope resolution operator
+	my_func::foo();
+	// using directive
+	foo();
 
 	exit (EXIT_SUCCESS);
 }
