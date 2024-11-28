ffdfc973d8de362726c4f3a0ad3dc4d801e30f62
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2024-11-28 21:35:24 +0100

    add sfml book example

---
+++ sfml/README.md
@@ -0,0 +1,11 @@
+Buch Beginning C++ Game Programming
+===================================
+
+Link zum Buch: t.b.d.
+Link zum Buch - Repo: t.b.d.
+
+
+additional stuff
+----------------
+
+([TODO](TODO.md))
---
+++ sfml/hello_world.cc
@@ -0,0 +1,28 @@
+/*
+ * hello world to check if everthing works
+ *
+ * taken from: https://www.sfml-dev.org/tutorials/2.6/start-linux.php
+ */
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
